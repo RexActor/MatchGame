bd881bc0a1340d48a3f3e2894c54b95e462f59f3
RexActor <bobsters.lol@gmail.com>
2022-10-09 13:30:48 +0100

    Timer Added

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,18 +13,40 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MatchGame
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
+	/// 
+	
 	public partial class MainWindow : Window
 	{
+		TextBlock lastTextBlockClicked;
+		bool findMatching = false;
+		DispatcherTimer timer = new DispatcherTimer();
+		int tenthofSecondsElapsed;
+		int matchesFound;
 		public MainWindow()
 		{
+			
 			InitializeComponent();
 			SetupGame();
+			timer.Interval = TimeSpan.FromSeconds(.1);
+			timer.Tick += Timer_Tick;
+		}
+
+		private void Timer_Tick(object sender, EventArgs e)
+		{
+			tenthofSecondsElapsed++;
+			timeTextBlock.Text = (tenthofSecondsElapsed / 10F).ToString("0.0s");
+			if (matchesFound == 8)
+			{
+				timer.Stop();
+				timeTextBlock.Text = timeTextBlock.Text + " - Play Again?";
+			}
 		}
 
 		private void SetupGame()
@@ -44,10 +67,39 @@ namespace MatchGame
 
 			foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>()) 
 			{
-				int index = random.Next(animalEmoji.Count);
-				string nextEmoji = animalEmoji[index];
-				textBlock.Text = nextEmoji;
-				animalEmoji.RemoveAt(index);
+				if (textBlock.Name != "timeTextBlock")
+				{
+					int index = random.Next(animalEmoji.Count);
+					string nextEmoji = animalEmoji[index];
+					textBlock.Text = nextEmoji;
+					animalEmoji.RemoveAt(index);
+				}
+			}
+			timer.Start();
+			tenthofSecondsElapsed = 0;
+			matchesFound = 0;
+
+		}
+
+		private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			TextBlock textBlock= sender as TextBlock;
+			if(findMatching == false)
+			{
+				textBlock.Visibility = Visibility.Hidden;
+				lastTextBlockClicked = textBlock;
+				findMatching = true;
+			}
+			else if (textBlock.Text == lastTextBlockClicked.Text)
+			{
+				matchesFound++;
+				textBlock.Visibility = Visibility.Hidden;
+				findMatching = false;
+			}
+			else
+			{
+				lastTextBlockClicked.Visibility = Visibility.Visible;
+				findMatching = false;
 			}
 
 		}
